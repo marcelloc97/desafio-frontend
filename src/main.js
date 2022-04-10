014edd1818c56e230879a21df069cfc94ca92dfa
Marcello Carvalhal <marcellocontact97@gmail.com>
2022-04-10 02:35:18 -0300

    wip feat: vuex configuration

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import Vuex from 'vuex';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
