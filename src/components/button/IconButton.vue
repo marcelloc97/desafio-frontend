4a8af56f7f5651b09b258365327a123795cb1a4f
Marcello Carvalhal <marcellocontact97@gmail.com>
2022-04-12 00:49:26 -0300

    feat: IconButton component

---
+++ src/components/button/IconButton.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <button>
+    <span class="material-icons md-18" v-text="icon" />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'IconButton',
+
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+button {
+  width: 30px;
+  height: 30px;
+  padding: 5px;
+  margin: 0;
+  outline: none;
+
+  border: none;
+  background: none;
+  border-radius: 50%;
+  text-align: center;
+  cursor: pointer;
+}
+button:hover {
+  background: #F5F6F8;
+}
+button:active {
+  background: #ebedf2;
+}
+</style>
